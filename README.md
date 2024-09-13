7f953bf05cd3c9a0b661eb48c190129d18e2046f
unilock <unilock@fennet.rentals>
2024-09-13 10:11:15 -0400

    init
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Remapper
+
+Yeah...
---
+++ src/main/java/cc/unilock/remapper/Main.java
@@ -0,0 +1,7 @@
+package cc.unilock.remapper;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
